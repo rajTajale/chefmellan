8acc4305f4de618faad941552fe254b01d913781
Raj Tajale <tajale60@gmail.com>
2021-08-23 10:25:20 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,242 @@
-import * as React from "react"
+import React from "react";
+import styled from "styled-components";
+import content from "../images/one.png";
+import grocery from "../images/kitchen.png";
+import chef from "../images/cook.png";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
-const IndexPage = () => {
+function IndexPage() {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
+    <MainWrapper>
+      <Content1>
+        <TextWrapper>
+          <span>Order your meals from local chefs,</span>
+          <br />
+          connecting everyone and building a community.
+        </TextWrapper>
+
+        <Image src={content} style={{ top: "50px" }} alt="pix" />
+      </Content1>
+      <Content2>
+        <TextWrapper2>HOW IT WORKS</TextWrapper2>
+        <MenuItems>
+          <Part1>
+            <Groceries src={grocery} style={{ top: "0" }} alt="pix"></Groceries>
+            Make the list of Groceries
+          </Part1>
+          <Part2>
+            <Chef src={chef} style={{ top: "0" }} alt="pix"></Chef>
+            Verified Chefs offers you meal plans
+          </Part2>
+          <Part3>Confirm the meal plan and Enjoy</Part3>
+        </MenuItems>
+      </Content2>
+    </MainWrapper>
+  );
 }
 
-export default IndexPage
+export default IndexPage;
+
+const Chef = styled.img`
+  margin-top: 20px;
+  height: 200px;
+  width: 200px;
+`;
+
+const Groceries = styled.img`
+  margin-top: 20px;
+  height: 200px;
+  width: 200px;
+`;
+
+const Part1 = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  text-align: center;
+  height: 400px;
+  width: 250px;
+  margin-top: 4%;
+`;
+
+const Part2 = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  text-align: center;
+  height: 400px;
+  width: 250px;
+  margin-top: 4%;
+`;
+
+const Part3 = styled.div`
+  background: yellow;
+  text-align: center;
+  height: 400px;
+  width: 250px;
+  margin-top: 4%;
+`;
+
+const MenuItems = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: space-around;
+  width: 80%;
+  height: 500px;
+  margin-top: 12%;
+  align-self: center;
+  margin-left: 10%;
+`;
+
+const Image = styled.img`
+  background-image: url("../images/two.png");
+  position: absolute;
+  justify-content: center;
+  display: grid;
+  left: 45%;
+  height: 600px;
+  width: 700px;
+  z-index: 1;
+`;
+
+const MainWrapper = styled.div`
+  display: grid;
+  position: relative;
+
+  height: 100%;
+  width: 100%;
+  font-size: 28px;
+  line-height: 1.47059;
+  font-weight: 800;
+  letter-spacing: 0.022em;
+  font-family: "Montserrat", "SF Pro Text", "SF Pro Icons", "Helvetica Neue",
+    "Helvetica", "Arial", sans-serif;
+  /* background-color: #fff; */
+  color: #1d1d1f;
+
+  @media (max-width: 450px) {
+    height: 100vh;
+    width: 100vw;
+  }
+  @media (max-width: 750px) {
+    height: 100vh;
+    width: 100vw;
+  }
+`;
+
+const Content1 = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: #f7f3ef;
+  @media (max-width: 450px) {
+    overflow: hidden;
+    width: 100vw;
+  }
+  @media (max-width: 750px) {
+    overflow-x: hidden;
+    width: 100vw;
+  }
+`;
+
+const TextWrapper = styled.div`
+  width: 40%;
+  height: 500px;
+  margin-top: 200px;
+  padding: 1em;
+  z-index: 3;
+
+  font-size: 28px;
+  line-height: 1.47059;
+  font-weight: 800;
+  letter-spacing: 0.022em;
+  font-family: "Montserrat", "SF Pro Text", "SF Pro Icons", "Helvetica Neue",
+    "Helvetica", "Arial", sans-serif;
+  /* background-color: #fff; */
+  color: #1d1d1f;
+  span {
+    text-shadow: 2px 2px black;
+    background: red; /* fallback for old browsers */
+    background: -webkit-linear-gradient(
+      to right,
+      red,
+      yellow
+    ); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(
+      to right,
+      orange,
+      lightblue
+    ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+    font-size: 33px;
+
+    @media (max-width: 450px) {
+      font-size: 35px;
+    }
+  }
+  @media (max-width: 450px) {
+    font-size: 32px;
+  }
+`;
+
+const TextWrapper2 = styled.div`
+  position: absolute;
+  margin-left: 40%;
+
+  width: 40%;
+  height: 500px;
+  margin-top: 30px;
+  padding: 1em;
+  z-index: 3;
+
+  font-size: 28px;
+  line-height: 1.47059;
+  font-weight: 800;
+  letter-spacing: 0.022em;
+  font-family: "Montserrat", "SF Pro Text", "SF Pro Icons", "Helvetica Neue",
+    "Helvetica", "Arial", sans-serif;
+  color: black;
+  span {
+    text-shadow: 2px 2px black;
+    background: red; /* fallback for old browsers */
+    background: -webkit-linear-gradient(
+      to right,
+      red,
+      yellow
+    ); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(
+      to right,
+      orange,
+      lightblue
+    ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+    font-size: 33px;
+
+    @media (max-width: 450px) {
+      font-size: 35px;
+    }
+  }
+  @media (max-width: 450px) {
+    font-size: 32px;
+  }
+`;
+
+const Content2 = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: whitesmoke;
+  @media (max-width: 450px) {
+    overflow: hidden;
+    width: 100vw;
+  }
+  @media (max-width: 750px) {
+    overflow-x: hidden;
+    width: 100vw;
+  }
+`;
